30623a534800eb49de29080ad938bb0d92a580e3
Kim Blanck <kimblanck@kims-air.home>
2021-02-05 18:08:23 -0500

    troubleshooting

--- app/models/glass.rb
+++ app/models/glass.rb
@@ -1,6 +1,11 @@
 class Glass < ApplicationRecord
 
-    DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'final_development'})
+    if(ENV['DATABASE_URL'])
+        uri = URI.parse(ENV['DATABASE_URL'])
+        DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+      else
+        DB = PG.connect(host: "localhost", port: 5432, dbname: 'final_development')
+      end
 
     def self.all
         results = DB.exec("SELECT * FROM glasses;")
