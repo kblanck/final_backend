f7a1fcbde1170c6e6c9edb346a21ff38794292e5
Kim Blanck <kimblanck@kims-air.home>
2021-02-08 13:31:13 -0500

    app rb

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module Final
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.initialize_on_precompile = false
+    
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
